b669f499b586e66574b9ea46cab9947eada528d0
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-10-21 21:27:09 +0300

    Заготовка для центрифуги

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,19 +1,2 @@
-$('input[id="send"]').click(function (e) {
-    e.preventDefault();
-
-    let publish = $('input[id="message-text"]').val()
-
-    $.ajax({
-        url: 'http://chat.api.loc/server.php',
-        method: 'POST',
-        data: {'publish': publish},
-        success: function (data){
-            console.log(data);
-        },
-        error: function ()
-        {
-            console.log('error');
-        }
-    });
-    $(this).val('');
-});
+import Centrifuge from 'centrifuge'
+import SockJS from 'sockjs-client'
