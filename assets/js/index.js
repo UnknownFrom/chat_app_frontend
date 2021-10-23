4c03503bd0ba9b50116811ef48aa39d01dc4c54e
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-10-23 18:43:32 +0300

    Добавлены комментарии и удалены лишние

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,16 +6,15 @@ const ws = new WebSocket("ws://127.0.0.1:8000");
 
 ws.onopen = () => {
     $.ajax({
-        url:"http://users.api.loc/token",
+        url: "http://users.api.loc/token",
         type: "POST",
         dataType: "JSON",
         data: {
             token: searchString.get('token')
         },
-        success(data){
-            if(!data.status)
-            {
-                document.location.href= '/auth';
+        success(data) {
+            if (!data.status) {
+                document.location.href = '/auth';
             }
             const mes = " подключился к чату";
             name = data.fullName;
@@ -28,9 +27,8 @@ ws.onopen = () => {
             userEl.appendChild(document.createTextNode(`Вы: ${name}`));
             usersListEl.appendChild(userEl);
         },
-        error: function ()
-        {
-            document.location.href= '/auth'
+        error: function () {
+            document.location.href = '/auth'
         }
     });
 }
@@ -40,19 +38,13 @@ ws.onmessage = (message) => {
     console.log(messages['name']);
     const messageEl = document.createElement('div');
     messageEl.appendChild(document.createTextNode(`${messages.name}: ${messages.mess}`));
-    /*if(messages.name) {
-        messageEl.appendChild(document.createTextNode(`${messages.name}: ${messages.mess}`));
-    }else{
-        messageEl.appendChild(document.createTextNode(`${messages.mess}`));
-    }*/
     chatEl.appendChild(messageEl);
     chatEl.scrollTo(0, chatEl.scrollHeight);
 
     removeUsers();
 
     const usersList = messages.usersList;
-    usersList.forEach((val) =>
-    {
+    usersList.forEach((val) => {
         const usersListEl = document.getElementById("users")
         const userEl = document.createElement('div');
         userEl.appendChild(document.createTextNode(val));
@@ -64,8 +56,7 @@ const send = (event) => {
     event.preventDefault();
     const mes = document.getElementById("message-text").value;
     document.getElementById("message-text").value = '';
-    if(mes === '' || name === '')
-    {
+    if (mes === '' || name === '') {
         return false;
     }
     ws.send(JSON.stringify({
@@ -73,15 +64,13 @@ const send = (event) => {
     }))
 }
 
-
-
 const formEl = document.getElementById("chat-form");
 formEl.addEventListener("submit", send);
 
 const logoutEl = document.getElementById("listUsers");
 logoutEl.addEventListener("submit", logout);
 
-function logout(event){
+function logout(event) {
     event.preventDefault();
     status = "offline";
     const mes = "отключается от чата";
@@ -93,7 +82,7 @@ function logout(event){
     return false;
 }
 
-/*ws.onclose = () => {
+ws.onclose = function (event) {
     console.log("Вышел");
     const mes = " отключился от чата";
     const status = 'offline';
@@ -101,22 +90,11 @@ function logout(event){
     ws.send(JSON.stringify({
         name, mes, status
     }))
-}*/
+};
 
-ws.onclose = function(event) {
-    console.log("Вышел");
-    const mes = " отключился от чата";
-    const status = 'offline';
-    //ws.close(1000, `${name} отключился от чата`);
-    ws.send(JSON.stringify({
-        name, mes, status
-    }))};
-
-function removeUsers(){
+function removeUsers() {
     const usersListEl = document.getElementById("users")
-    while (usersListEl.firstChild)
-    {
+    while (usersListEl.firstChild) {
         usersListEl.removeChild(usersListEl.firstChild);
     }
-
 }
