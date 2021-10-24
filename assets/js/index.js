01e960906ec75bc5707abe25ccdaea696bc7f4b5
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-10-24 15:00:10 +0300

    Реализована база данных сообщений

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,16 @@ const chatEl = document.getElementById("chat");
 const ws = new WebSocket("ws://127.0.0.1:8000");
 
 ws.onopen = () => {
+    /*let response = await fetch("http://users.api.loc/token",{
+        mode: 'no-cors',
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({token: searchString.get('token')})
+    });
+    let json = await response.json();
+    console.log(json);*/
     $.ajax({
         url: "http://users.api.loc/token",
         type: "POST",
@@ -16,16 +26,17 @@ ws.onopen = () => {
             if (!data.status) {
                 document.location.href = '/auth';
             }
-            const mes = " подключился к чату";
+            const message = " подключился к чату";
             name = data.fullName;
             status = 'online';
+            const event = 'add_user';
             ws.send(JSON.stringify({
-                name, mes, status
+                name, message, status, event
             }))
-            const usersListEl = document.getElementById("users")
+            /*const usersListEl = document.getElementById("users")
             const userEl = document.createElement('div');
             userEl.appendChild(document.createTextNode(`Вы: ${name}`));
-            usersListEl.appendChild(userEl);
+            usersListEl.appendChild(userEl);*/
         },
         error: function () {
             document.location.href = '/auth'
@@ -35,41 +46,46 @@ ws.onopen = () => {
 
 ws.onmessage = (message) => {
     const messages = JSON.parse(message.data);
-    console.log(messages['name']);
-    const messageEl = document.createElement('div');
-    messageEl.appendChild(document.createTextNode(`${messages.name}: ${messages.mess}`));
-    chatEl.appendChild(messageEl);
-    chatEl.scrollTo(0, chatEl.scrollHeight);
+    console.log(messages);
+    for(const val of messages)
+    {
+        const messageEl = document.createElement('div');
+        messageEl.appendChild(document.createTextNode(`${val.fullName}: ${val.message}`));
+        chatEl.appendChild(messageEl);
+        chatEl.scrollTo(0, chatEl.scrollHeight);
+    }
 
     removeUsers();
-
-    const usersList = messages.usersList;
-    usersList.forEach((val) => {
+/*    const json = JSON.parse(messages.usersList)
+    console.log(json);*/
+    for(const val of messages[0].usersList)
+    {
+        console.log(val);
         const usersListEl = document.getElementById("users")
         const userEl = document.createElement('div');
         userEl.appendChild(document.createTextNode(val));
         usersListEl.appendChild(userEl);
-    })
+    }
 }
 
 window.onunload = function(){
     status = "offline";
-    const mes = "отключается от чата";
+    const message = "отключается от чата";
     ws.send(JSON.stringify({
-        name, mes, status
+        name, message, status
     }))
     ws.close();
 };
 
 const send = (event) => {
     event.preventDefault();
-    const mes = document.getElementById("message-text").value;
+    const message = document.getElementById("message-text").value;
     document.getElementById("message-text").value = '';
-    if (mes === '' || name === '') {
+    if (message === '' || name === '') {
         return false;
     }
     ws.send(JSON.stringify({
-        name, mes, status
+        name, message, status
     }))
 }
 
@@ -82,9 +98,9 @@ logoutEl.addEventListener("submit", logout);
 function logout(event) {
     event.preventDefault();
     status = "offline";
-    const mes = "отключается от чата";
+    const message = "отключается от чата";
     ws.send(JSON.stringify({
-        name, mes, status
+        name, message, status
     }))
     //ws.close();
     window.location.href = "/";
