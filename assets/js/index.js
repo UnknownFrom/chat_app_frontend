9dd0f815b1d66f7fd1bf1ed856040291067b9cff
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-10-27 19:07:04 +0300

    Изменён стиль чата

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 let name, _event, id, status;
-const chatEl = document.getElementById("chat");
-const ws = new WebSocket("ws://127.0.0.1:8000");
+const chatEl = document.getElementById('chat');
+const ws = new WebSocket('ws://127.0.0.1:8000');
 
 ws.onopen = () => {
     if (!localStorage.getItem('token')) {
@@ -13,52 +13,60 @@ ws.onopen = () => {
 
 ws.onmessage = (message) => {
     const messages = JSON.parse(message.data, reviver);
-    console.log(messages);
-    let messageEl;
     for (const message of messages) {
         if (!message.event) {
-            message.event = "send_message";
+            message.event = 'send_message';
         }
         switch (message.event) {
             case 'add_user':
+                /* записываем данные текущего пользователя */
                 id = message.id;
                 name = message.fullName;
+                /* вывод активных пользователей и сообщения о входе */
                 printUsers(message.usersList);
-                messageEl = document.createElement('div');
-                messageEl.appendChild(document.createTextNode(`${message.fullName} ${message.message}`));
-                messageEl.style.textAlign = 'center';
-                messageEl.style.marginBottom = '5px';
-                chatEl.appendChild(messageEl);
-                chatEl.scrollTo(0, chatEl.scrollHeight);
+                printInfoMessage(message)
                 break;
             case 'disconnect':
+                /* закрытие соединения для дублированных вкладок */
                 ws.send(JSON.stringify({token: localStorage.getItem('token'), _event: 'check_token'}))
-                //window.localStorage.clear();
                 printUsers(message.usersList);
-                messageEl = document.createElement('div');
-                messageEl.appendChild(document.createTextNode(`${message.fullName} ${message.message}`));
-                messageEl.style.textAlign = 'center';
-                messageEl.style.marginBottom = '5px';
-                chatEl.appendChild(messageEl);
-                chatEl.scrollTo(0, chatEl.scrollHeight);
+                printInfoMessage(message);
                 break;
             case 'send_message':
-                messageEl = document.createElement('div');
-                messageEl.appendChild(document.createTextNode(`${message.fullName}: ${message.message}`));
-                if (name === message.fullName) {
-                    messageEl.style.textAlign = "right"
-                }
-                chatEl.appendChild(messageEl);
-                chatEl.scrollTo(0, chatEl.scrollHeight);
+                sendMessage(message);
                 break;
         }
     }
 }
 
+function sendMessage(message)
+{
+    const timeEl = document.createElement('div');
+    timeEl.appendChild(document.createTextNode(`${message.time}`));
+    timeEl.classList.add('message_time');
+
+    const messageEl = document.createElement('div');
+    messageEl.appendChild(document.createTextNode(`${message.fullName}: ${message.message}`));
+
+    const messageBlock = document.createElement('div');
+    messageBlock.classList.add('message_block');
+    messageBlock.appendChild(timeEl);
+    messageBlock.appendChild(messageEl);
+    if (name === message.fullName) {
+        messageBlock.classList.add('message_self', 'message_color_self', 'message_block_self');
+    }
+    else{
+        messageBlock.classList.add('message_another', 'message_color_another', 'message_block_another');
+    }
+    chatEl.appendChild(messageBlock);
+    chatEl.scrollTo(0, chatEl.scrollHeight);
+}
+
+
 window.onbeforeunload = function () {
-    status = "offline";
-    _event = "disconnect";
-    const message = "отключается от чата";
+    status = 'offline';
+    _event = 'disconnect';
+    const message = 'отключается от чата';
     ws.send(JSON.stringify({
         id, message, _event
     }))
@@ -67,9 +75,9 @@ window.onbeforeunload = function () {
 
 const send = (event) => {
     event.preventDefault();
-    _event = "send_message";
-    const message = document.getElementById("message-text").value;
-    document.getElementById("message-text").value = '';
+    _event = 'send_message';
+    const message = document.getElementById('message-text').value;
+    document.getElementById('message-text').value = '';
     if (message === '' || name === '') {
         return false;
     }
@@ -79,17 +87,17 @@ const send = (event) => {
     }))
 }
 
-const formEl = document.getElementById("chat-form");
-formEl.addEventListener("submit", send);
+const formEl = document.getElementById('chat-form');
+formEl.addEventListener('submit', send);
 
-const logoutEl = document.getElementById("listUsers");
-logoutEl.addEventListener("submit", logout);
+const logoutEl = document.getElementById('listUsers');
+logoutEl.addEventListener('submit', logout);
 
 function logout(event) {
     event.preventDefault();
-    status = "offline";
-    _event = "disconnect";
-    const message = "отключается от чата";
+    status = 'offline';
+    _event = 'disconnect';
+    const message = 'отключается от чата';
     ws.send(JSON.stringify({
         id, message, _event
     }))
@@ -98,13 +106,23 @@ function logout(event) {
     //localStorage.removeItem('token');
     ws.close();
     localStorage.removeItem('token');
-    window.location.href = "http://chat.loc/";
+    window.location.href = 'http://chat.loc/';
 
     return true;
 }
 
+function printInfoMessage(message)
+{
+    const messageEl = document.createElement('div');
+    messageEl.appendChild(document.createTextNode(`${message.fullName} ${message.message}`));
+    messageEl.style.textAlign = 'center';
+    messageEl.style.marginBottom = '5px';
+    chatEl.appendChild(messageEl);
+    chatEl.scrollTo(0, chatEl.scrollHeight);
+}
+
 function removeUsers() {
-    const usersListEl = document.getElementById("users")
+    const usersListEl = document.getElementById('users')
     while (usersListEl.firstChild) {
         usersListEl.removeChild(usersListEl.firstChild);
     }
@@ -122,7 +140,7 @@ function reviver(key, value) {
 function printUsers(data) {
     removeUsers();
     for (const val of data) {
-        const usersListEl = document.getElementById("users")
+        const usersListEl = document.getElementById('users')
         const userEl = document.createElement('div');
         userEl.style.marginBottom = '5px';
         if (val[1] === name) {
